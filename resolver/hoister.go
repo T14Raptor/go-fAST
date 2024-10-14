b60e8842b1364198035f1773a3ade96c2cedf677
T14Raptor <t14raptor@gmail.com>
2024-10-14 15:07:34 -0400

    improvements

--- resolver/hoister.go
+++ resolver/hoister.go
@@ -1,16 +1,21 @@
 package resolver
 
-import "github.com/t14raptor/go-fast/ast"
+import (
+	"github.com/t14raptor/go-fast/ast"
+	"github.com/t14raptor/go-fast/token"
+)
 
-/*
-	Loosely inspired from https://rustdoc.swc.rs/swc_ecma_transforms_base/fn.resolver.html
-*/
+// Loosely inspired from https://rustdoc.swc.rs/swc_ecma_transforms_base/fn.resolver.html
 
 type Hoister struct {
-	resolver          *Resolver
-	kind              DeclKind
-	inBlock           bool
-	inCatchBody       bool
+	ast.NoopVisitor
+
+	resolver *Resolver
+	kind     DeclKind
+	inBlock  bool
+
+	inCatchBody bool
+
 	excludedFromCatch map[string]struct{}
 	catchParamDecls   map[string]struct{}
 }
@@ -19,70 +24,73 @@ func NewHoister(resolver *Resolver) *Hoister {
 	return &Hoister{
 		resolver:          resolver,
 		kind:              DeclKindVar,
-		inBlock:           false,
-		inCatchBody:       false,
 		excludedFromCatch: make(map[string]struct{}),
 		catchParamDecls:   make(map[string]struct{}),
-	}
-}
-
-func (h *Hoister) addPatId(id *ast.Identifier) {
-	if h.inCatchBody {
-		if _, ok := h.catchParamDecls[id.Name]; ok {
-			r, _ := h.resolver.markForRef(id.Name)
-			if r != UnresolvedMark {
-				return
-			}
-		}
-		h.excludedFromCatch[id.Name] = struct{}{}
-	} else {
-		if _, ok := h.catchParamDecls[id.Name]; ok {
-			if _, excluded := h.excludedFromCatch[id.Name]; !excluded {
-				return
-			}
-		}
-	}
-	h.resolver.modify(id, h.kind)
-}
-
-func (h *Hoister) VisitVariableStatement(n *ast.VariableDeclaration) {
-	if h.inBlock {
-		//if n.Kind != ast.VarKind {
-		//	return
-		//}
-	}
-
-	oldKind := h.kind
-	h.kind = DeclKindVar
-	for _, decl := range n.List {
-		h.VisitVariableDeclarator(decl)
-	}
-	h.kind = oldKind
-}
-
-func (h *Hoister) VisitVariableDeclarator(n *ast.VariableDeclarator) {
-	if ident, ok := n.Target.Target.(*ast.Identifier); ok {
-		h.addIdent(ident)
 	}
 }
 
 func (h *Hoister) addIdent(id *ast.Identifier) {
 	if h.inCatchBody {
 		if _, ok := h.catchParamDecls[id.Name]; ok {
-			r, _ := h.resolver.markForRef(id.Name)
-			if r != UnresolvedMark {
+			if r, _ := h.resolver.markForRef(id.Name); r != UnresolvedMark {
 				return
 			}
 		}
+
 		h.excludedFromCatch[id.Name] = struct{}{}
-	} else {
-		if _, ok := h.catchParamDecls[id.Name]; ok {
-			if _, excluded := h.excludedFromCatch[id.Name]; !excluded {
-				return
-			}
+	} else if _, ok := h.catchParamDecls[id.Name]; ok {
+		if _, excluded := h.excludedFromCatch[id.Name]; !excluded {
+			return
 		}
 	}
+
 	h.resolver.modify(id, h.kind)
+}
+
+func (h *Hoister) VisitBlockStatement(n *ast.BlockStatement) {
+	old := h.inBlock
+	h.inBlock = true
+	n.VisitChildrenWith(h)
+	h.inBlock = old
+}
+
+func (h *Hoister) VisitStatements(n *ast.Statements) {
+	others := make(ast.Statements, 0, len(*n))
+	for i := range *n {
+		switch it := (*n)[i].Stmt.(type) {
+		case *ast.VariableDeclaration:
+			it.VisitWith(h)
+		case *ast.FunctionDeclaration:
+			it.VisitWith(h)
+		default:
+			others = append(others, (*n)[i])
+		}
+	}
+
+	for i := range others {
+		others[i].VisitWith(h)
+	}
+}
+
+func (h *Hoister) VisitVariableDeclaration(n *ast.VariableDeclaration) {
+	if h.inBlock {
+		if n.Token != token.Var {
+			return
+		}
+	}
+
+	oldKind := h.kind
+	h.kind = DeclKindVar
+	n.VisitChildrenWith(h)
+	h.kind = oldKind
+}
+
+func (h *Hoister) VisitBindingTarget(n *ast.BindingTarget) {
+	if ident, ok := n.Target.(*ast.Identifier); ok {
+		h.addIdent(ident)
+		return
+	}
+	n.VisitChildrenWith(h)
 }
 
 func (h *Hoister) VisitFunctionDeclaration(n *ast.FunctionDeclaration) {
@@ -92,11 +100,24 @@ func (h *Hoister) VisitFunctionDeclaration(n *ast.FunctionDeclaration) {
 
 	if h.inBlock {
 		if symbol := h.resolver.current.findVarInfo(n.Function.Name.Name); symbol != nil {
-			//if symbol.DeclType == DeclKindLexical || symbol.DeclType == DeclKindParam {
-			//	return
-			//}
+			if symbol.DeclKind != DeclKindVar && symbol.DeclKind != DeclKindFunction {
+				return
+			}
 		}
 	}
 
 	h.resolver.modify(n.Function.Name, DeclKindFunction)
 }
+
+func (h *Hoister) VisitSwitchStatement(n *ast.SwitchStatement) {
+	n.Discriminant.VisitWith(h)
+
+	old := h.inBlock
+	h.inBlock = true
+	n.Body.VisitWith(h)
+	h.inBlock = old
+}
+
+func (h *Hoister) VisitArrowFunctionLiteral(n *ast.ArrowFunctionLiteral) {}
+func (h *Hoister) VisitExpression(n *ast.Expression)                     {}
+func (h *Hoister) VisitFunctionLiteral(n *ast.FunctionLiteral)           {}
--- resolver/resolver.go
+++ resolver/resolver.go
@@ -8,9 +8,8 @@ import (
 type IdentType int
 
 const (
-	IdentTypeRef      IdentType = iota // Reference (read)
-	IdentTypeBinding                   // Binding (declaration)
-	IdentTypeRefWrite                  // Reference (write)
+	IdentTypeRef     IdentType = iota // Reference (read)
+	IdentTypeBinding                  // Binding (declaration)
 )
 
 type DeclKind int
@@ -18,7 +17,6 @@ type DeclKind int
 const (
 	DeclKindVar DeclKind = iota
 	DeclKindFunction
-	DeclKindClass
 )
 
 type ScopeKind int
@@ -26,10 +24,10 @@ type ScopeKind int
 const (
 	ScopeKindBlock ScopeKind = iota
 	ScopeKindFunction
-	ScopeKindForStatement
 )
 
 type VarInfo struct {
+	DeclKind   DeclKind
 	Identifier *ast.Identifier
 	Value      *ast.Expression
 }
@@ -78,134 +76,13 @@ func NewResolver() *Resolver {
 	}
 	r.V = r
 
-	r.pushScope(ScopeKindBlock) // Global scope
 	return r
 }
 
-func (r *Resolver) VisitBlockStatement(n *ast.BlockStatement) {
-	r.pushScope(ScopeKindBlock)
-	r.hoistScope(n.List)
-	n.VisitChildrenWith(r)
-	r.popScope()
-}
-
-func (r *Resolver) VisitForOfStatement(n *ast.ForOfStatement) {
-	r.pushScope(ScopeKindBlock) // Using Block scope for ForOfStatement
-
-	oldIdentType := r.identType
-
-	// Handle the 'Into' part (left-hand side of for...of)
-	if n.Into != nil {
-		r.identType = IdentTypeBinding
-		switch into := n.Into.Into.(type) {
-		case *ast.VariableDeclaration:
-			// Handle ForIntoVar (likely a simple variable)
-			for _, x := range into.List {
-				r.VisitBindingTarget(x.Target)
-			}
-
-		//case *ast.ForDeclaration:
-		// Handle ForDeclaration (likely a 'let' or 'const' declaration)
-
-		//for _, decl := range into.Target. {
-		//	r.VisitVariableDeclarator(decl)
-		//}
-		default:
-			// Log unexpected type or handle error
-			fmt.Printf("Unexpected ForInto type: %T\n", into)
-		}
-	}
-
-	// Handle the 'Source' part (right-hand side of for...of)
-
-	if n.Source != nil {
-		r.VisitExpression(n.Source)
-	}
-
-	// Handle body
-	r.identType = oldIdentType
-	if n.Body != nil {
-		//if blockStmt, ok := n.Body.Stmt.(*ast.BlockStatement); ok {
-		//	//r.markBlock(&blockStmt.ScopeContext)
-		//}
-		n.Body.VisitWith(r)
-	}
-	r.identType = IdentTypeRef
-	r.popScope()
-}
-
-//func (r *Resolver) VisitCallExpression(n *ast.CallExpression) {
-//	for _, x := range
-//}
-
-func (r *Resolver) VisitForStatement(n *ast.ForStatement) {
-	r.pushScope(ScopeKindBlock) // Using Block scope as ForStatement is not defined
-
-	oldIdentType := r.identType
-
-	// Handle initializer
-	if n.Initializer != nil {
-		r.identType = IdentTypeBinding
-		if s, ok := n.Initializer.Initializer.(*ast.VariableDeclaration); ok {
-			for _, x := range s.List {
-				r.VisitBindingTarget(x.Target)
-				r.identType = IdentTypeRef
-				r.VisitExpression(x.Initializer)
-			}
-		} else if expr, ok := n.Initializer.Initializer.(*ast.Expression); ok {
-			r.identType = IdentTypeRef
-			r.VisitExpression(expr)
-		}
-	}
-
-	// Handle test expression
-	r.identType = IdentTypeRef
-	if n.Test != nil {
-		r.VisitExpression(n.Test)
-	}
-
-	// Handle update expression
-	if n.Update != nil {
-		r.VisitExpression(n.Update)
-	}
-
-	// Handle body
-	r.identType = oldIdentType
-
-	n.Body.VisitWith(r)
-
-	r.popScope()
-}
-
-func (r *Resolver) VisitFunctionDeclaration(n *ast.FunctionDeclaration) {
-	r.modify(n.Function.Name, DeclKindFunction)
-	r.VisitFunctionLiteral(n.Function)
-}
-
-func (r *Resolver) VisitFunctionLiteral(n *ast.FunctionLiteral) {
-	r.pushScope(ScopeKindFunction)
-
-	oldIdentType := r.identType
-	r.identType = IdentTypeBinding
-	for _, param := range n.ParameterList.List {
-		r.VisitBindingTarget(param.Target)
-	}
-
-	if n.ParameterList.Rest != nil {
-		switch rest := n.ParameterList.Rest.(type) {
-		case *ast.Identifier:
-			r.VisitIdentifier(rest)
-		default:
-			panic(fmt.Sprintf("Unexpected rest type: %T\n", n.ParameterList.Rest))
-		}
-	}
-
-	r.identType = oldIdentType
-
-	r.hoistScope(n.Body.List)
-	n.Body.VisitChildrenWith(r)
-
-	r.popScope()
+func Resolve(p *ast.Program) *Resolver {
+	resolver := NewResolver()
+	p.VisitWith(resolver)
+	return resolver
 }
 
 func (r *Resolver) newCtxt() ast.ScopeContext {
@@ -248,156 +125,140 @@ func (r *Resolver) markForRef(sym string) (ast.ScopeContext, *Scope) {
 	return UnresolvedMark, nil
 }
 
+func (r *Resolver) VisitBlockStatement(n *ast.BlockStatement) {
+	r.pushScope(ScopeKindBlock)
+	n.VisitChildrenWith(r)
+	r.popScope()
+}
+
+func (r *Resolver) VisitForOfStatement(n *ast.ForOfStatement) {
+	r.pushScope(ScopeKindBlock) // Using Block scope for ForOfStatement
+
+	oldIdentType := r.identType
+	r.identType = IdentTypeRef
+
+	// Handle the 'Into' part (left-hand side of for...of)
+	n.Into.VisitWith(r)
+
+	// Handle the 'Source' part (right-hand side of for...of)
+	n.Source.VisitWith(r)
+
+	//if blockStmt, ok := n.Body.Stmt.(*ast.BlockStatement); ok {
+	//	//r.markBlock(&blockStmt.ScopeContext)
+	//}
+	n.Body.VisitWith(r)
+
+	r.identType = oldIdentType
+	r.popScope()
+}
+
+func (r *Resolver) VisitForStatement(n *ast.ForStatement) {
+	r.pushScope(ScopeKindBlock) // Using Block scope as ForStatement is not defined
+
+	oldIdentType := r.identType
+	r.identType = IdentTypeBinding
+
+	// Handle initializer
+	if n.Initializer != nil {
+		n.Initializer.VisitWith(r)
+	}
+
+	// Handle test expression
+	r.identType = IdentTypeRef
+	n.Test.VisitWith(r)
+
+	// Handle update expression
+	n.Update.VisitWith(r)
+
+	// Handle body
+	r.identType = oldIdentType
+	n.Body.VisitWith(r)
+
+	r.popScope()
+}
+
+func (r *Resolver) VisitFunctionDeclaration(n *ast.FunctionDeclaration) {
+	r.modify(n.Function.Name, DeclKindFunction)
+
+	n.Function.VisitWith(r)
+}
+
+func (r *Resolver) VisitFunctionLiteral(n *ast.FunctionLiteral) {
+	r.pushScope(ScopeKindFunction)
+
+	oldIdentType := r.identType
+	r.identType = IdentTypeBinding
+	n.ParameterList.VisitWith(r)
+
+	if rest, ok := n.ParameterList.Rest.(*ast.Identifier); ok {
+		rest.VisitWith(r)
+	} else if rest != nil {
+		panic(fmt.Sprintf("Unexpected rest type: %T\n", n.ParameterList.Rest))
+	}
+
+	r.identType = oldIdentType
+
+	n.Body.VisitChildrenWith(r)
+
+	r.popScope()
+}
+
 func (r *Resolver) VisitProgram(n *ast.Program) {
-	r.hoistScope(n.Body)
+	r.pushScope(ScopeKindBlock)
+	n.VisitChildrenWith(r)
+	r.popScope()
+}
+
+func (r *Resolver) VisitStatements(n *ast.Statements) {
+	// Handle hoisting
+	h := NewHoister(r)
+	h.V = h
+	n.VisitWith(h)
+
+	// Resolve
 	n.VisitChildrenWith(r)
 }
 
-func (r *Resolver) hoistStatement(stmt ast.Statement, hoister *Hoister) {
-	switch s := stmt.Stmt.(type) {
-	case *ast.VariableDeclaration:
-		hoister.VisitVariableStatement(s)
-	case *ast.FunctionDeclaration:
-		hoister.VisitFunctionDeclaration(s)
-	case *ast.BlockStatement:
-		oldInBlock := hoister.inBlock
-		hoister.inBlock = true
-		for _, blockStmt := range s.List {
-			r.hoistStatement(blockStmt, hoister)
-		}
-		hoister.inBlock = oldInBlock
-	}
-}
-
-func (r *Resolver) hoistScope(stmts []ast.Statement) {
-	hoister := NewHoister(r)
-	for _, stmt := range stmts {
-		r.hoistStatement(stmt, hoister)
-	}
-}
-
-func (r *Resolver) VisitVariableStatement(n *ast.VariableDeclaration) {
+func (r *Resolver) VisitVariableDeclaration(n *ast.VariableDeclaration) {
 	oldDeclKind := r.declKind
 	r.declKind = DeclKindVar
 
 	for _, decl := range n.List {
-		r.VisitVariableDeclarator(decl)
+		oldIdentType := r.identType
+		r.identType = IdentTypeBinding
+		decl.Target.VisitWith(r)
+		r.identType = oldIdentType
+
+		decl.Initializer.VisitChildrenWith(r)
 	}
 
 	r.declKind = oldDeclKind
-}
-
-func (r *Resolver) VisitBindingTarget(target *ast.BindingTarget) {
-	switch t := target.Target.(type) {
-	case *ast.Identifier:
-		r.VisitIdentifier(t)
-	case *ast.ArrayPattern:
-		for _, element := range t.Elements {
-			if element.Expr != nil {
-				r.VisitBindingTarget(element.Expr.(*ast.BindingTarget))
-			}
-		}
-	case *ast.ObjectPattern:
-	//fmt.Println(1)
-	//for _, prop := range t.Properties {
-	//	a := (prop).(*ast.Property)
-	//	r.VisitBindingTarget(*prop.(*ast.Property)))
-	//}
-	default:
-		fmt.Println(1)
-	}
 }
 
 func (r *Resolver) VisitExpression(expr *ast.Expression) {
 	if expr == nil || expr.Expr == nil {
 		return
 	}
-	switch e := expr.Expr.(type) {
-	case *ast.Identifier:
-		r.VisitIdentifier(e)
-	case *ast.AssignExpression:
-		r.VisitAssignExpression(e)
-	default:
-		expr.VisitChildrenWith(r)
-	}
-}
 
-func (r *Resolver) VisitAssignmentExpression(n *ast.AssignExpression) {
 	oldIdentType := r.identType
-
-	// lhs is a write reference
-	r.identType = IdentTypeRefWrite
-	r.VisitExpression(n.Left)
-
-	// rhs is a read reference, so we need a reference type
 	r.identType = IdentTypeRef
-	r.VisitExpression(n.Right)
-
+	expr.VisitChildrenWith(r)
 	r.identType = oldIdentType
 }
 
 func (r *Resolver) VisitIdentifier(n *ast.Identifier) {
-	if n == nil {
+	if n == nil || n.ScopeContext != UnresolvedMark {
 		return
 	}
 
 	switch r.identType {
 	case IdentTypeBinding:
 		r.modify(n, r.declKind)
-	case IdentTypeRef, IdentTypeRefWrite:
-		if n.ScopeContext != UnresolvedMark {
-			return
-		}
-
+	case IdentTypeRef:
 		if mark, _ := r.markForRef(n.Name); mark != UnresolvedMark {
 			n.ScopeContext = mark
 		} else {
-			n.ScopeContext = UnresolvedMark
-		}
-
-		if scope := r.findInOuterScopes(n.Name); scope != nil {
-			n.ScopeContext = scope.mark
-		} else {
-			n.ScopeContext = UnresolvedMark
+			r.modify(n, r.declKind)
 		}
 	}
-}
-
-func (r *Resolver) findInOuterScopes(name string) *Scope {
-	for scope := r.current; scope != nil; scope = scope.parent {
-		if _, exists := scope.declaredSymbols[name]; exists {
-			return scope
-		}
-	}
-	return nil
-}
-
-/* Handle classes */
-func (r *Resolver) VisitFieldDefinition(n *ast.FieldDefinition) {
-	// Handle field definition
-	if n.Key != nil {
-		(*n.Key).VisitWith(r)
-	}
-	if n.Initializer != nil {
-		r.VisitExpression(n.Initializer)
-	}
-}
-
-func (r *Resolver) VisitMethodDefinition(n *ast.MethodDefinition) {
-	// Handle method definition
-	if n.Key != nil {
-		(*n.Key).VisitWith(r)
-	}
-	r.VisitFunctionLiteral(n.Body)
-}
-
-func (r *Resolver) VisitClassStaticBlock(n *ast.ClassStaticBlock) {
-	// Handle static block
-	r.VisitBlockStatement(n.Block)
-}
-
-func Resolve(p *ast.Program) *Resolver {
-	resolver := NewResolver()
-	p.VisitWith(resolver)
-	return resolver
 }
