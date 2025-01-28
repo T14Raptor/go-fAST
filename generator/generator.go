a162ac9d35bf15524dac8c4634839393ecdc66a2
robinsamuel <robin.s.jung@outlook.com>
2025-01-28 23:59:15 +0100

    fix: add parentheses for functions in member expr object

--- generator/generator.go
+++ generator/generator.go
@@ -263,7 +263,8 @@ func (g *GenVisitor) VisitDoWhileStatement(n *ast.DoWhileStatement) {
 
 func (g *GenVisitor) VisitMemberExpression(n *ast.MemberExpression) {
 	switch n.Object.Expr.(type) {
-	case *ast.AssignExpression, *ast.BinaryExpression, *ast.UnaryExpression, *ast.SequenceExpression, *ast.ConditionalExpression, *ast.NumberLiteral:
+	case *ast.AssignExpression, *ast.BinaryExpression, *ast.UnaryExpression, *ast.SequenceExpression, *ast.ConditionalExpression, *ast.NumberLiteral,
+		*ast.FunctionLiteral, *ast.ArrowFunctionLiteral:
 		g.out.WriteString("(")
 		g.gen(n.Object.Expr)
 		g.out.WriteString(")")
