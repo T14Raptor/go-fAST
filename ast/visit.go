9ef31941dbfa244bca4f7aa48d82c09fd1fc41fe
GitHub Action <action@github.com>
2025-10-29 11:59:48 +0000

    Auto-generate visit.go

--- ast/visit.go
+++ ast/visit.go
@@ -64,7 +64,6 @@ type Visitor interface {
 	VisitPropertyKeyed(n *PropertyKeyed)
 	VisitPropertyShort(n *PropertyShort)
 	VisitRegExpLiteral(n *RegExpLiteral)
-	VisitRemoveVisitor(n *RemoveVisitor)
 	VisitReturnStatement(n *ReturnStatement)
 	VisitSequenceExpression(n *SequenceExpression)
 	VisitSpreadElement(n *SpreadElement)
@@ -276,9 +275,6 @@ func (nv *NoopVisitor) VisitPropertyShort(n *PropertyShort) {
 	n.VisitChildrenWith(nv.V)
 }
 func (nv *NoopVisitor) VisitRegExpLiteral(n *RegExpLiteral) {
-	n.VisitChildrenWith(nv.V)
-}
-func (nv *NoopVisitor) VisitRemoveVisitor(n *RemoveVisitor) {
 	n.VisitChildrenWith(nv.V)
 }
 func (nv *NoopVisitor) VisitReturnStatement(n *ReturnStatement) {
@@ -776,12 +772,6 @@ func (n *RegExpLiteral) VisitWith(v Visitor) {
 	v.VisitRegExpLiteral(n)
 }
 func (n *RegExpLiteral) VisitChildrenWith(v Visitor) {
-}
-func (n *RemoveVisitor) VisitWith(v Visitor) {
-	v.VisitRemoveVisitor(n)
-}
-func (n *RemoveVisitor) VisitChildrenWith(v Visitor) {
-	n.NoopVisitor.VisitWith(v)
 }
 func (n *ReturnStatement) VisitWith(v Visitor) {
 	v.VisitReturnStatement(n)
